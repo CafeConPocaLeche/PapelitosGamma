473c7d00eda97214a098f4c9d77224f2ba4136ea
CafeConPocaLeche <sergio.surink@gmail.com>
2021-07-12 18:34:22 +0200

    Pantalla intermedia

---
+++ app/src/main/java/com/example/papelitosgamma/Activities/startRound.java
@@ -0,0 +1,24 @@
+package com.example.papelitosgamma.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.papelitosgamma.R;
+
+public class startRound extends AppCompatActivity {
+
+    public static String roundName = "Ronda 1";
+    private TextView mTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_round);
+
+        mTV = findViewById(R.id.roundName);
+        mTV.setText(roundName);
+
+    }
+}
