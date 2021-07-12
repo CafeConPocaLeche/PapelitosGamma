a7911ff832b638786f61e39fbfcf103d42bf9119
CafeConPocaLeche <sergio.surink@gmail.com>
2021-07-12 19:39:26 +0200

    Pantalla intermedia

--- app/src/main/java/com/example/papelitosgamma/Activities/startRound.java
+++ app/src/main/java/com/example/papelitosgamma/Activities/startRound.java
@@ -3,6 +3,7 @@ package com.example.papelitosgamma.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.TextView;
 
 import com.example.papelitosgamma.R;
@@ -10,7 +11,8 @@ import com.example.papelitosgamma.R;
 public class startRound extends AppCompatActivity {
 
     public static String roundName = "Ronda 1";
-    private TextView mTV;
+    private TextView mTV, mTVC;
+    public int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,21 @@ public class startRound extends AppCompatActivity {
 
         mTV = findViewById(R.id.roundName);
         mTV.setText(roundName);
+
+        mTVC = findViewById(R.id.countDown);
+        new CountDownTimer(3000,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTVC.setText(String.valueOf(counter));
+                counter++;
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
 
     }
 }
